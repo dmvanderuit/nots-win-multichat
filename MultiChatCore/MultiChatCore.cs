c1a6fa74917158d88d866f8f75afcb0c8194fbf7
danielvanderuit <dm.vanderuit@student.han.nl>
2021-02-16 12:48:26 +0100

    Add working stuff again

--- MultiChatCore/MultiChatCore.cs
+++ MultiChatCore/MultiChatCore.cs
@@ -1,6 +1,8 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using AppKit;
 using MultiChatCore.model;
 using Newtonsoft.Json;
 
@@ -20,6 +22,33 @@ namespace MultiChatCore
         {
             var buffer = Encoding.ASCII.GetBytes(message.ToJson() + "@");
             await ns.WriteAsync(buffer, 0, buffer.Length);
+        }
+    }
+
+    public class UI
+    {
+        public static void ShowAlert(string title, string message)
+        {
+            var alert = new NSAlert()
+            {
+                AlertStyle = NSAlertStyle.Critical,
+                MessageText = title,
+                InformativeText = message
+            };
+            alert.RunModal();
+        }
+
+        public static void ShowExceptionAlert(string title, Exception exception)
+        {
+            var messageSb = new StringBuilder("The following error occured: ");
+            messageSb.Append(exception.Message);
+            var alert = new NSAlert()
+            {
+                AlertStyle = NSAlertStyle.Critical,
+                MessageText = title,
+                InformativeText = messageSb.ToString()
+            };
+            alert.RunModal();
         }
     }
 }
